54c0c7d782f7dc2e22860e80c84ffda2c6f52382
Kunal Dhillon <kusdhill@gmail.com>
2019-10-16 10:18:07 -0700

    spaces to tabs

--- MaximumProductSubarray.java
+++ MaximumProductSubarray.java
@@ -1,18 +1,18 @@
 class MaximumProductSubarray{
 	public static int maxProduct(int[] nums){
 		int curMax = nums[0];
-        int curMin = nums[0];
-        int totMax = nums[0];
-        
-        int temp;
-        
-        for(int i=1; i<nums.length; i++){
-            temp = curMax;
-            curMax = Math.max(nums[i], Math.max(nums[i]*curMax, nums[i]*curMin));
-            curMin = Math.min(nums[i], Math.min(nums[i]*curMin, nums[i]*temp));
-            totMax = Math.max(curMax, totMax);
-        }
-        return totMax;
+		int curMin = nums[0];
+		int totMax = nums[0];
+		
+		int temp;
+		
+		for(int i=1; i<nums.length; i++){
+			temp = curMax;
+			curMax = Math.max(nums[i], Math.max(nums[i]*curMax, nums[i]*curMin));
+			curMin = Math.min(nums[i], Math.min(nums[i]*curMin, nums[i]*temp));
+			totMax = Math.max(curMax, totMax);
+		}
+		return totMax;
 	}
 
 	public static void main(String args[]){
