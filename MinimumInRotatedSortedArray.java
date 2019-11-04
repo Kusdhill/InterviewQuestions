c7d3da90d008fb6954b8307090328df64e2bda99
Kunal Dhillon <kusdhill@gmail.com>
2019-11-04 11:05:31 -0800

    better example case

--- MinimumInRotatedSortedArray.java
+++ MinimumInRotatedSortedArray.java
@@ -25,7 +25,7 @@ class MinimumInRotatedSortedArray{
 	}
 
 	public static void main(String args[]){
-		int[] nums = {1,2,3};
+		int[] nums = {4,5,6,7,0,1,2};
 		System.out.println(findMin(nums));
 	}
 }
