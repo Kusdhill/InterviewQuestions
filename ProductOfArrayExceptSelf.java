9768f777466c58a083097d052ca45106bb20148d
Kunal Dhillon <kusdhill@gmail.com>
2019-11-04 15:12:08 -0800

    constant space solution

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -31,8 +31,35 @@ class ProductOfArrayExceptSelf{
 
 	}
 
+	public static int[] productOfArrayExceptSelfConstantSpace(int[] nums){
+		int n = nums.length;
+
+		if(n<2){
+			return new int[] {0};
+		}
+
+		int[] res = new int[n];
+
+		res[0] = 1;
+
+		for(int i=1; i<n; i++){
+			res[i] = res[i-1]*nums[i-1];
+		}
+
+		int right = 1;
+
+		for(int i=n-1; i>-1; i--){
+			res[i] *= right;
+			right *= nums[i];
+		}
+
+		return res;
+
+	}
+
 	public static void main(String args[]){
 		int[] nums = {1,2,3,4};
 		System.out.println(Arrays.toString(productOfArrayExceptSelf(nums)));
+		System.out.println(Arrays.toString(productOfArrayExceptSelfConstantSpace(nums)));
 	}
 }
