e506ea14f272defb8d912b6d182cb23f8502ba17
Kunal Dhillon <kusdhill@gmail.com>
2019-11-06 14:07:30 -0800

    non-recursive binary search

---
+++ BinarySearch.java
@@ -0,0 +1,36 @@
+class BinarySearch{
+	public static int binarySearch(int[] nums, int target){
+		
+		if(nums==null || nums.length==0){
+			return -1;
+		}
+
+		int n = nums.length;
+
+		int left=0, right=n-1;
+		int mid;
+		
+		while(left<right){
+			mid = left + (right-left)/2;
+
+			if(nums[mid]==target){
+				return mid;
+			}
+
+			if(target<mid){
+				right = mid-1;
+			}else{
+				left = mid+1;
+			}
+		}
+
+		return -1;
+
+	}
+
+	public static void main(String args[]){
+		int[] nums = {0,1,2,3,4,5,6,7};
+		int target = 6;
+		System.out.println(binarySearch(nums, target));
+	}
+}
