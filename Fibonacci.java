40836cb602afc52122c63adfc0d72b02f89980d1
Kunal Dhillon <kusdhill@gmail.com>
2019-11-14 19:51:21 -0800

    a terrible recursive solution I will make more efficient later

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+class Fibonnacci{
+	public static int fibonnaci(int n){
+
+		if(n<=1){
+			return n;
+		}
+
+		return fibonnaci(n-1) + fibonnaci(n-2);
+	}
+
+	public static void main(String[] args){
+		System.out.println(fibonnaci(10));
+	}
+}
