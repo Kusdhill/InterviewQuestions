3d66e3fd32524fba61d404dbee5c2e4ee8d10954
Kunal Dhillon <kusdhill@gmail.com>
2019-11-15 08:16:28 -0800

    more efficient memoized solution

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,26 @@
 class Fibonnacci{
+
 	public static int fibonnaci(int n){
+		if(n<=1){
+			return n;
+		}
+
+		return memoize(n);
+	}
+
+	public static int memoize(int n){
+		int[] cache = new int[n+1];
+		cache[0] = 0;
+		cache[1] = 1;
+
+		for(int i=2; i<=n; i++){
+			cache[i] = cache[i-1] + cache[i-2];
+		}
+
+		return cache[n];
+	}
+
+	public static int recursiveFibonnaci(int n){
 
 		if(n<=1){
 			return n;
@@ -9,6 +30,7 @@ class Fibonnacci{
 	}
 
 	public static void main(String[] args){
-		System.out.println(fibonnaci(10));
+		System.out.println(fibonnaci(4));
+		System.out.println(recursiveFibonnaci(4));
 	}
 }
