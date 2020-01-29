c4592cae680fbee5a84fb3e7a6264457daae1ca3
Kunal Dhillon <kusdhill@gmail.com>
2020-01-29 06:57:01 -0800

    print statement and new test case

--- WordSearch.java
+++ WordSearch.java
@@ -1,6 +1,18 @@
 class WordSearch{
 	
+	private static void printBoard(char[][] board){
+		for(int i=0; i<board.length; i++){
+			for(int j=0; j<board[i].length; j++){
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
 	private static boolean exist(int y, int x, int i, int j, char[][] board, String word, int wordPos){
+		System.out.println(Integer.toString(i)+", "+Integer.toString(j));
+		printBoard(board);
 		// if reached end of word, return true
 		if(wordPos>=word.length()){
 			return true;
@@ -9,12 +21,12 @@ class WordSearch{
 		if(i<0 || i>=y || j<0 || j>=x || board[i][j]=='*' || word.charAt(wordPos)!=board[i][j]){
 			return false;
 		}
-
+		
 		board[i][j] = '*';
-		boolean result = 	exist(y, x, i-1, j, board, word, wordPos+1)
-							|| exist(y, x, i+1, j, board, word, wordPos+1)
-							|| exist(y, x, i, j-1, board, word, wordPos+1)
-							|| exist(y, x, i, j+1, board, word, wordPos+1);		
+		boolean result = 	exist(y, x, i-1, j, board, word, wordPos+1) // up
+							|| exist(y, x, i+1, j, board, word, wordPos+1) // down
+							|| exist(y, x, i, j-1, board, word, wordPos+1) // left
+							|| exist(y, x, i, j+1, board, word, wordPos+1); // right
 		board[i][j] = word.charAt(wordPos);
 
 		return result;
@@ -37,12 +49,12 @@ class WordSearch{
 
 	public static void main(String[] args){
 		char[][] board = {
-  			{'A','B','C','E'},
-  			{'S','F','C','S'},
-  			{'A','D','E','E'}
+  			{'A','B','C','S'},
+  			{'S','F','A','F'},
+  			{'A','C','E','E'}
 		};
 
-		String word = "ABCCEDS";
+		String word = "SFC";
 		System.out.println(exist(board, word));
 	}
 }
